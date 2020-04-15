5453f13ea868c5ecbb0c97a200341d2b0489da23
cbu <dev@macboiss.com>
2020-04-15 23:41:44 +0200

    - hide change password for non admin

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -170,8 +170,10 @@ func (hs *HTTPServer) setIndexViewData(c *m.ReqContext) (*dtos.IndexViewData, er
 			HideFromMenu: true,
 			Children: []*dtos.NavLink{
 				{Text: "Preferences", Id: "profile-settings", Url: setting.AppSubUrl + "/profile", Icon: "gicon gicon-preferences"},
-				{Text: "Change Password", Id: "change-password", Url: setting.AppSubUrl + "/profile/password", Icon: "fa fa-fw fa-lock", HideFromMenu: true},
 			},
+		}
+		if c.OrgRole != m.ROLE_VIEWER {
+			profileNode.Children = append(profileNode.Children, &dtos.NavLink				{Text: "Change Password", Id: "change-password", Url: setting.AppSubUrl + "/profile/password", Icon: "fa fa-fw fa-lock", HideFromMenu: true})
 		}
 
 		if !setting.DisableSignoutMenu {
