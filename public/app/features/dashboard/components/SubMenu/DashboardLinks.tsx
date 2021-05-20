8d6844da7840b31436070ee49dd0c0bd70fc855c
cbu <dev@macboiss.com>
2021-05-20 18:01:22 +0200

    - links popup video

--- public/app/features/dashboard/components/SubMenu/DashboardLinks.tsx
+++ public/app/features/dashboard/components/SubMenu/DashboardLinks.tsx
@@ -7,6 +7,8 @@ import { getLinkSrv } from '../../../panel/panellinks/link_srv';
 import { DashboardModel } from '../../state';
 import { DashboardLink } from '../../state/DashboardModel';
 import { iconMap } from '../DashLinks/DashLinksEditorCtrl';
+import { appEvents } from 'app/core/app_events';
+import { CoreEvents } from 'app/types';
 
 export interface Props {
   dashboard: DashboardModel;
@@ -14,9 +16,63 @@ export interface Props {
 }
 
 export const DashboardLinks: FC<Props> = ({ dashboard, links }) => {
+  const videoDetector = 'vimeo:';
+
   if (!links.length) {
     return null;
   }
+
+  function getVideoId(linkUrl: string) {
+    return linkUrl.substring(videoDetector.length, linkUrl.length);
+  }
+
+  function checkToDisplayVideo(links: DashboardLink[]) {
+    try {
+      links.forEach((link: DashboardLink) => {
+        if (link.url.startsWith(videoDetector)) {
+          let videoId = getVideoId(link.url);
+
+          if (!getCookie('video-' + videoId)) {
+            displayModal(link.url, link.title);
+          }
+        }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function displayModal(linkUrl: string, linkTitle: string) {
+    let videoId = getVideoId(linkUrl);
+    let videoUrl = 'https://player.vimeo.com/video/' + videoId + '?title=0&byline=0&portrait=0';
+    appEvents.emit(CoreEvents.showModal, {
+      src: 'public/custom/video-popup.html',
+      model: {
+        id: videoId,
+        url: videoUrl,
+        title: linkTitle,
+      },
+      modalClass: 'video',
+    });
+  }
+
+  function getCookie(cname: string) {
+    var name = cname + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
+  }
+
+  checkToDisplayVideo(links);
 
   return (
     <>
@@ -28,12 +84,26 @@ export const DashboardLinks: FC<Props> = ({ dashboard, links }) => {
           return <DashboardLinksDashboard key={key} link={link} linkInfo={linkInfo} dashboardId={dashboard.id} />;
         }
 
-        const linkElement = (
-          <a className="gf-form-label" href={sanitizeUrl(linkInfo.href)} target={link.targetBlank ? '_blank' : '_self'}>
-            <Icon name={iconMap[link.icon] as IconName} style={{ marginRight: '4px' }} />
-            <span>{sanitize(linkInfo.title)}</span>
-          </a>
-        );
+        let linkElement: JSX.Element;
+        if (link.url.startsWith(videoDetector)) {
+          linkElement = (
+            <button className="gf-form-label" onClick={() => displayModal(link.url, link.title)}>
+              <Icon name={iconMap[link.icon] as IconName} style={{ marginRight: '4px' }} />
+              <span>{sanitize(linkInfo.title)}</span>
+            </button>
+          );
+        } else {
+          linkElement = (
+            <a
+              className="gf-form-label"
+              href={sanitizeUrl(linkInfo.href)}
+              target={link.targetBlank ? '_blank' : '_self'}
+            >
+              <Icon name={iconMap[link.icon] as IconName} style={{ marginRight: '4px' }} />
+              <span>{sanitize(linkInfo.title)}</span>
+            </a>
+          );
+        }
 
         return (
           <div key={key} className="gf-form">
